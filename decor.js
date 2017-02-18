d8c04f31e85eae83557e3c27866dc550fcf5ac8a
enidmwayuli <mwayulienid@gmail.com>
2017-02-18 10:43:30 +0300

    Home Interior

---
+++ decor.js
@@ -0,0 +1,12 @@
+document.getElementById("nav01").innerHTML =
+"<ul>" +
+// "<li><a href='index.html'>Home</a></li>" +
+"<li><a href='purchase.php'>Make an Order</a></li>"+
+"<li><a href='tips.html'>Home Care Tips</a></li>" +
+"<li><a href='accessories.html'>Home Accessories</a></li>" +
+"<li><a href='kitchen.html'>Kitchen</a></li>" +
+"<li><a href='bedroom.html'>Bedroom</a></li>"+
+"<li><a href='livingroom.html'>Livingroom</a></li>"+
+"</ul>";
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Melz interiors. All rights reserved.</p>";
---
+++ purchase.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+</style>
+<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<title></title>
+</head>
+<body>
+<div id="header">
+
+	<table border="0" >
+	<tr >
+         <td> <img src="assets/images/1.jpg" width="100px" height="100px"></td><td> <h3 style="color: #C80000">Melz Interiors</h3> </td>
+    </tr>
+    
+    </table>
+</div>
+<nav id="nav01"></nav>
+
+<div id="social-media">
+
+<a href="https://www.facebook.com/r.php?profile_id=20531316728&next=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2F&friend_or_subscriber=friend" target= "_blank" > <img src="assets/images/facebook.png" width="40px" height="40px" /> </a>
+
+<a href="https://twitter.com/signup" target="_blank"><img src="assets/images/twitter.png" width="40px" height="40px" /> </a>
+
+</div>
+
+
+<div id="register" align="center">
+
+<p class="order" ><strong>MAKE AN ORDER</strong></p>
+<form name="customer" method="POST" action="details.php" enctype="multipart/form-data">
+<br>
+Full Name <br>		<input type="text" name="fname"/><p></p>
+Product Code <br>		<input type="text" name="lname"/><p></p>
+Phone Number <br>		<input type="text" name="number"/><p></p>
+Email address <br>  <input type="text" name="email"/><p></p>
+<!-- Password  <br>      <input type="password" name="password"/><p></p>
+Confirm Password <br><input type="password" name="passwordConfirm"/><p></p>
+Photo  <br>         <input type="file" name="image"/><p></p> -->
+<input type="submit" name="submit" value="Submit Order" />
+
+</form>
+</div>
+
+<footer id="foot01"></footer>
+
+<script src="decor.js"></script>
+</body>
+</html>
